1d0ab91aa27a16761fa6652ac894f1be9aabf79b
jowonyoung1 <jwy7254@naver.com>
2024-04-04 13:08:59 +0900

    할 일 앱 만들기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
+import { useRef, useState } from "react";
 import TodoInsert from "./components/ToDoinsert";
 import Todolist from "./components/TodoList";
 import TodoTemplate from "./components/TodoTemplate";
+
+const initState = [
+  {
+    id: 1,
+    text: "리액트의 기초 알아보기",
+    checked: true,
+  },
+  {
+    id: 2,
+    text: "컴포넌트의 이해",
+    checked: false,
+  },
+  {
+    id: 3,
+    text: "할 일 앱 만들어 보기",
+    checked: true,
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(initState);
+
+  // id는 고유한 값이어야 한다.
+  const nextId = useRef(4);
+
+  // todos 배열에 새 객채 추가
+  const onInsert = text => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos(todos.concat(todo));
+    nextId.current += 1;
+  };
+
+  // 삭제 기능
+  const onRemove = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  // 토글 기능
+  const onToggle = id => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    );
+  };
+
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <Todolist />
+      <TodoInsert onInsert={onInsert} />
+      <Todolist todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 }
--- src/components/ToDoinsert.js
+++ src/components/ToDoinsert.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { MdAdd } from "react-icons/md";
 import "../styles/TodoInsert.scss";
-const TodoInsert = () => {
+
+const TodoInsert = ({ onInsert }) => {
   const [value, setValue] = useState("");
 
   const onChange = event => {
@@ -9,11 +10,14 @@ const TodoInsert = () => {
   };
 
   const onSubmit = event => {
+    onInsert(value);
+
     // submit 이벤트는 브라우저에서 새로고침 발생시킴
     // 이를 방지하기 위해 아래 함수 호출
     event.preventDefault();
     setValue("");
   };
+
   return (
     <form className="TodoInsert" onSubmit={onSubmit}>
       <input
